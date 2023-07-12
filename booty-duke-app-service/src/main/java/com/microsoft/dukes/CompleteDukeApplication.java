1fb13d61abb023405b91f0180d428029d7e82a1e
riyazu159 <shaikriyazu159@gmail.com>
2023-07-12 11:07:45 -0400

    Revert "Update CompleteDukeApplication.java"
    This reverts commit 138428ad789cd6dc006d05ee2163b2c6ad8a20f8.

--- booty-duke-app-service/src/main/java/com/microsoft/dukes/CompleteDukeApplication.java
+++ booty-duke-app-service/src/main/java/com/microsoft/dukes/CompleteDukeApplication.java
@@ -8,9 +8,23 @@ import jakarta.ws.rs.core.Application;
 @ApplicationPath("")
 public class CompleteDukeApplication extends Application {
 
-    public String main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-        return "Hi this is riyaz";
+        Application app = new CompleteDukeApplication();
+        SeBootstrap.Configuration config = SeBootstrap.Configuration.builder()
+                .rootPath("")
+                .host("0.0.0.0")
+                .port(80) // default port for app service
+                .protocol("HTTP")
+                .build();
+
+        SeBootstrap.start(app, config).thenAccept(instance -> {
+            instance.stopOnShutdown(stopResult -> stopResult.unwrap(Object.class));
+            System.out.printf("\nBooty Duke running at %s\n", instance.configuration().baseUri());
+            System.out.println("Send SIGKILL to shutdown.");
+        });
+
+        Thread.currentThread().join();
     }
 
 }
